71744c72b0f7b70e2e6da2e3dc07692a19d153d2
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-12-27 16:34:00 +0800

    feat: collpase code block

--- src/components/markdown/CodeRenderer.tsx
+++ src/components/markdown/CodeRenderer.tsx
@@ -1,10 +1,22 @@
+import { ChevronsUpDown } from "lucide-react";
 import {
   CodeBlock,
   CodeBlockBody,
   CodeBlockItem,
   CodeBlockContent,
   BundledLanguage,
+  CodeBlockCopyButton,
+  CodeBlockFilename,
+  CodeBlockFiles,
+  CodeBlockHeader,
+  CodeBlockSelect,
+  CodeBlockSelectContent,
+  CodeBlockSelectItem,
+  CodeBlockSelectTrigger,
+  CodeBlockSelectValue,
 } from "../ui/shadcn-io/code-block";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export type CodeRendererProps = {
   language: string;
@@ -12,22 +24,63 @@ export type CodeRendererProps = {
 };
 
 export default function CodeRenderer({ language, content }: CodeRendererProps) {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <CodeBlock
       data={[
         {
           language: language,
-          filename: "",
+          filename: "Scratch",
           code: content,
         },
       ]}
       defaultValue={language}
     >
+      <CodeBlockHeader>
+        <CodeBlockFiles>
+          {(item) => (
+            <CodeBlockFilename key={item.language} value={item.language}>
+              {item.filename}
+            </CodeBlockFilename>
+          )}
+        </CodeBlockFiles>
+        <CodeBlockSelect>
+          <CodeBlockSelectTrigger>
+            <CodeBlockSelectValue />
+          </CodeBlockSelectTrigger>
+          <CodeBlockSelectContent>
+            {(item) => (
+              <CodeBlockSelectItem key={item.language} value={item.language}>
+                {item.language}
+              </CodeBlockSelectItem>
+            )}
+          </CodeBlockSelectContent>
+        </CodeBlockSelect>
+        <button
+          className={cn(
+            "rounded-full hover:bg-background p-1 mx-1",
+            collapsed && "bg-background",
+          )}
+          onClick={() => setCollapsed(!collapsed)}
+        >
+          <div>
+            <ChevronsUpDown className="size-4 text-muted-foreground" />
+          </div>
+        </button>
+        <CodeBlockCopyButton
+          onCopy={() => console.log("Copied code to clipboard")}
+          onError={() => console.error("Failed to copy code to clipboard")}
+        />
+      </CodeBlockHeader>
+
       <CodeBlockBody>
         {(item) => (
           <CodeBlockItem key={item.language} value={item.language}>
             <CodeBlockContent language={item.language as BundledLanguage}>
-              {item.code}
+              {!collapsed
+                ? item.code
+                : "Code collpased. Click the button to expand."}
             </CodeBlockContent>
           </CodeBlockItem>
         )}
